fd950a1ebdae4e9b93278865ef94ce6328325f92
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-02-07 18:28:04 +0100

    improved cookie tests

--- tests/specs/lib/cookie.js
+++ tests/specs/lib/cookie.js
@@ -11,12 +11,11 @@ describe('odl/lib/cookie', () => {
     windowSpy = {
       bla: 'blubb',
       document: {
-        cookie: 'foo=bar;test=3598235',
+        cookie: '',
       },
     };
     // register mocks
     mockModule('odl/lib/globals/window', windowSpy);
-    // clear domHelper first, then import
     System.delete(System.normalizeSync('odl/lib/cookie'));
     return System.import('odl/lib/cookie').then((m) => {
       cookieHelper = m.default;
@@ -25,6 +24,7 @@ describe('odl/lib/cookie', () => {
 
   describe('get', () => {
     it('should get a cookie with a given name and return its value', () => {
+      windowSpy.document.cookie = 'foo=bar;test=3598235';
       assert.equal(cookieHelper.get('foo'), 'bar');
       assert.equal(cookieHelper.get('test'), '3598235');
     });
@@ -32,16 +32,36 @@ describe('odl/lib/cookie', () => {
 
   describe('set', () => {
     it('should set a cookie with a given name to a given value', () => {
-      windowSpy.document.cookie = '';
       cookieHelper.set('xyz', 'foofoofoo');
       assert.include(windowSpy.document.cookie, 'xyz=foofoofoo');
+    });
+
+    it('should restrict a cookie to a given path using options.path', () => {
+      cookieHelper.set('xyz', 'foofoofoo', { path: '/' });
+      assert.include(windowSpy.document.cookie, 'xyz=foofoofoo;path=/');
+    });
+
+    it('should restrict a cookie to a given domain using options.domain', () => {
+      cookieHelper.set('xyz', 'foofoofoo', { domain: 'foo.bar.com' });
+      assert.include(windowSpy.document.cookie, 'xyz=foofoofoo;domain=foo.bar.com');
+    });
+
+    it('should set the max-age of a cookie using options.maxAge', () => {
+      cookieHelper.set('xyz', 'foofoofoo', { maxAge: 123456 });
+      assert.include(windowSpy.document.cookie, 'xyz=foofoofoo;max-age=123456');
+    });
+
+    it('should set the expiry date of a cookie using options.expires', () => {
+      cookieHelper.set('xyz', 'foofoofoo', { expires: '2016-12-22T00:00:00' });
+      assert.include(windowSpy.document.cookie, 'xyz=foofoofoo;expires=2016-12-22T00:00:00');
     });
   });
 
   describe('remove', () => {
     it('should remove a cookie with a given name', () => {
+      windowSpy.document.cookie = 'foo=bar;';
       cookieHelper.remove('foo');
-      assert.include(windowSpy.document.cookie, 'foo=;');
+      assert.include(windowSpy.document.cookie, 'foo=null;');
     });
   });
 });
